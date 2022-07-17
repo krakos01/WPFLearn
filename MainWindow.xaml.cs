31df50be0dcd919abbe76d1185ed0b38d643c584
krakos01 <dkrajewski001@outlook.com>
2022-07-17 18:09:30 +0200

    Rich Text controls e1:  -FlowDocument[Scroll/Page]ViewerControl

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,13 +31,6 @@ namespace Tooler
 			InitializeComponent();
 		}
 
-		private void txtEditor_SelectionChanged(object sender, RoutedEventArgs e)
-		{
-			int row = txtEditor.GetLineIndexFromCharacterIndex(txtEditor.CaretIndex);
-			int col = txtEditor.CaretIndex - txtEditor.GetCharacterIndexFromLineIndex(row);
-			lblCursorPosition.Text = "Line " + (row + 1) + ", Char " + (col + 1);
-			
-			Path.Text = DateTime.Now.ToString();
-		}
+
 	}
 }
