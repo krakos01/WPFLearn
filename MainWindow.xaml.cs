8371b73ea703b6bb1cb96d3c9e58ecbedd9c7198
krakos01 <dkrajewski001@outlook.com>
2022-07-18 11:46:14 +0200

    Rich text controls 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -31,6 +31,34 @@ namespace Tooler
 			InitializeComponent();
 		}
 
+		private void btnGetText_Click(object sender, RoutedEventArgs e)
+		{
+			TextRange textRange = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+			MessageBox.Show(textRange.Text);
+		}
 
+		private void btnSetText_Click(object sender, RoutedEventArgs e)
+		{
+			TextRange textRange = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+			textRange.Text = "Another world, another text!";
+		}
+
+		private void btnGetSelectedText_Click(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(rtbEditor.Selection.Text);
+		}
+
+		private void btnSetSelectedText_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Selection.Text = "[Replaced text]";
+		}
+
+		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			TextRange tempRange = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Selection.Start);
+			txtStatus.Text = "Selection starts at character #" + tempRange.Text.Length + Environment.NewLine;
+			txtStatus.Text += "Selection is " + rtbEditor.Selection.Text.Length + " character(s) long" + Environment.NewLine;
+			txtStatus.Text += "Selected text: '" + rtbEditor.Selection.Text + "'";
+		}
 	}
 }
